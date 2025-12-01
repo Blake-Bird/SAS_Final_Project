d326018d858fe65c6f7fb166f33e41de2a8550ea
Blake_Bird <bbird1@highpoint.edu>
2025-12-01 17:56:23 -0500

    Remove unused CSS import from App.jsx
    Removed unused CSS import from App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-// src/App.jsx
 import React from 'react';
 import PresentationNvda from './PresentationNvda.jsx';
-import './styles_nvda.css';
 
 export default function App() {
   return <PresentationNvda />;
