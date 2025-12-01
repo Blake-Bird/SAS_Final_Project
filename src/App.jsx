8f2cebe8bef2e33b39e784a4b58ef950f6f8347a
Blake_Bird <bbird1@highpoint.edu>
2025-12-01 15:13:59 -0500

    Create App component to render PresentationNvda

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+// src/App.jsx
+import React from 'react';
+import PresentationNvda from './PresentationNvda.jsx';
+import './styles_nvda.css';
+
+export default function App() {
+  return <PresentationNvda />;
+}
